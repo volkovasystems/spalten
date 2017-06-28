0e295f5222cd4434f4b3c57bed9457bd6a83f5a3
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-28 15:27:16 +0800

    Updated dependency.

--- spalten.js
+++ spalten.js
@@ -34,10 +34,11 @@
 			"file": "spalten.js",
 			"module": "spalten",
 			"author": "Richeve S. Bebedor",
-			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-			],
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/spalten.git",
 			"global": true
 		}
