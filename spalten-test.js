2db96f1f91c5b2444b11ad873b567379256b1147
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-28 15:26:48 +0800

    Updated server/client test.

--- spalten-test.js
+++ spalten-test.js
@@ -1,5 +1,9 @@
-"use strict";
 
+const assert = require( "assert" );
 const spalten = require( "./spalten.js" );
 
-console.log( spalten( 4, 2 ) )
+assert.equal( typeof spalten( 4, 2 ) == "object", true, "should be true" );
+
+assert.deepEqual( spalten( 5, 2 ), { "size": 3, "count": 2, "factor": 1.3763819204711736 }, "should be deeply equal" );
+
+console.log( "ok" );
