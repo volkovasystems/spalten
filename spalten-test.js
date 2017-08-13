431e66eb1b4879bc41858cd2d69feae3e41d337e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 15:03:13 +0800

    Updated test.

--- spalten-test.js
+++ spalten-test.js
@@ -2,8 +2,9 @@
 const assert = require( "assert" );
 const spalten = require( "./spalten.js" );
 
-assert.equal( typeof spalten( 4, 2 ) == "object", true, "should be true" );
+assert.equal( typeof spalten( 4, 2 ) == "object", true, "should be equal to true" );
 
-assert.deepEqual( spalten( 5, 2 ), { "size": 3, "count": 2, "factor": 1.3763819204711736 }, "should be deeply equal" );
+assert.deepEqual( spalten( 5, 2 ), { "size": 3, "count": 2, "factor": 1.3763819204711736 },
+	"should be equal to { 'size': 3, 'count': 2, 'factor': 1.3763819204711736 }" );
 
 console.log( "ok" );
